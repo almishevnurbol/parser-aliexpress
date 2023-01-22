fbdb05d4de3d0216f4247ce189934df438581228
Almishev Nurbol <almishev.01@gmail.com>
2023-01-22 21:44:15 +0600

    window onload

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ const login = async (url) => {
     let page = await browser.newPage();
 
     await page.goto(url);
+    await page.waitForSelector('.gallery_Gallery__picture__re6q0q', {
+      visible: true,
+    })
+
     result = await page.content()
 
     return result
